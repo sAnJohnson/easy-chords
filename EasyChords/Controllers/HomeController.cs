7e7bcd72b0e260a486a6bef2cd120b5c29212c53
Steven Johnson <stevenjohnson@musicalnoise.co.uk>
2020-07-07 22:34:31 +0100

    Added home page

---
+++ EasyChords/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyChords.Models;
+using EasyChords.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyChords.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IChordRepository _chordRepository;
+
+        public HomeController(IChordRepository chordRepository)
+        {
+            _chordRepository = chordRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                MinorChords = _chordRepository.MinorChords,
+                MajorChords = _chordRepository.MajorChords
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ EasyChords/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EasyChords.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyChords.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Chord> MajorChords { get; set; }
+        public IEnumerable<Chord> MinorChords { get; set; }
+    }
+}
